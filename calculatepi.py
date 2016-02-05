970181edaa20a9ac3d6f52d6e0edab7af67d4f32
Hagin Onyango <hagincubes@gmail.com>
2016-02-05 10:09:40 -0500

    Updated from Brython Server: 2/5/2016 10:09:39 AM

--- calculatepi.py
+++ calculatepi.py
@@ -1,10 +1,2 @@
-myPi = 4.0
-a = 3.0
-
-user = input("I will estimate pi. How many terms should I use?")
-
-while a < 10 : 
-    myPi = myPi - (4/a) + (4/(a+2))
-    a += 4 
-    print(myPi)
+import math 
 
