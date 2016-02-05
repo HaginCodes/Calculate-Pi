9104fd90ea9d9b61431f2c982b318a3cc9310a6b
Hagin Onyango <hagincubes@gmail.com>
2016-02-05 14:57:07 -0500

    Updated from Brython Server: February 5, 2016 2:57:06 PM EST

--- calculatepi.py
+++ calculatepi.py
@@ -1,4 +1,10 @@
-import math 
+"""
+calculatepi.py
+Author: Hagin Onyango
+Credit: Teacher
+
+""" 
+math 
 n = int(input("I will estimate pi. How many terms should I use? "))
 decimals = int(input("How many decimal places should I use in the result? "))
 a = 4.0*sum([((-1.0)**k)/(2*(k) + 1) for k in range(0,n)])
